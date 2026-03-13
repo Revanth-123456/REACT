16ae29341b61a91e70ff5d28b31723b497237f07
Revanth-123456 <revanthvs04@gmail.com>
2026-03-13 09:35:36 +0530

    commit

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -3,36 +3,37 @@ import Sample from './Component/March10/Sample';
 import Movies from './Component/March10/Movies';
 import ProductCard from './Component/March10/ProductCard';
 import FirstComponent from './FirstComponent';
-
+import ClassOne from './Component/March10/Class';
+import Payment from './Component/March10/Payment';
 
 function App() {
 
 
- const products = [
-  {
-    id: 1,
-    sponsored: true,
-    title: "Google Pixel 9A (Porcelain, 256 GB)",
-    image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/0/q/f/-original-imahadxg8mgjzrfh.jpeg?q=70",
-    rating: 4.5,
-    reviews: "8,289",
-    reviewCount: "548",
-    features: [
-      "8 GB RAM | 256 GB ROM",
-      "15.96 cm (6.28 inch) Full HD+ Display",
-      "48MP + 12MP | 12MP Front Camera",
-      "5100 mAh Battery",
-      "Tensor G4 Processor",
-      "1 Year Domestic Warranty",
-    ],
-    price: "39,999",
-    oldPrice: "49,999",
-    discount: 20,
-    exchange: "Upto ₹32,900 Off on Exchange",
-    bankOffer: "Bank Offer",
-    assured: true,
-  },
-];
+//  const products = [
+//   {
+//     id: 1,
+//     sponsored: true,
+//     title: "Google Pixel 9A (Porcelain, 256 GB)",
+//     image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/0/q/f/-original-imahadxg8mgjzrfh.jpeg?q=70",
+//     rating: 4.5,
+//     reviews: "8,289",
+//     reviewCount: "548",
+//     features: [
+//       "8 GB RAM | 256 GB ROM",
+//       "15.96 cm (6.28 inch) Full HD+ Display",
+//       "48MP + 12MP | 12MP Front Camera",
+//       "5100 mAh Battery",
+//       "Tensor G4 Processor",
+//       "1 Year Domestic Warranty",
+//     ],
+//     price: "39,999",
+//     oldPrice: "49,999",
+//     discount: 20,
+//     exchange: "Upto ₹32,900 Off on Exchange",
+//     bankOffer: "Bank Offer",
+//     assured: true,
+//   },
+// ];
 
 //   let movieData =[
 //     {
@@ -45,7 +46,7 @@ function App() {
 
 
 
-  let name = "David";
+  // let name = "David";
 
   // let obj = [
   //   {name:"David",employer:"Google"},
@@ -61,11 +62,13 @@ function App() {
   return (
     <div>
       {/* <Sample obj={obj}/> */}
-      <Sample firstname={name} />
+      {/* <Sample firstname={name} /> */}
       {/* <Movies movies={movieData}/> */}
       {/* <ProductCard products={products} /> */}
       {/* <h1>hello</h1> */}
       {/* <FirstComponent></FirstComponent>  */}
+      <ClassOne name="David" />
+      <Payment/>
     </div>
   );
 }
